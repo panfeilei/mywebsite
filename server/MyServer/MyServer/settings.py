87524d12c30a956437886a1790eba030de4c0229
notuglyboy <569925642@qq.com>
2018-02-11 18:11:55 +0800

    f

--- server/MyServer/MyServer/settings.py
+++ server/MyServer/MyServer/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 MEDIA_URL="/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,'MyServer','media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_URL = '/static/'
