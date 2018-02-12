6f09a6620a80da05d5d199339063c84764b141ee
notuglyboy <569925642@qq.com>
2018-02-12 15:51:04 +0800

    'd'

--- server/MyServer/testapp/admin.py
+++ server/MyServer/testapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from testapp.models import MyUser
+from testapp.models import MyUser, Blog
 # Register your models here.
 admin.site.register(MyUser)
+admin.site.register(Blog)
