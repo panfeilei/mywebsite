f6dd5401cf8b9336371df9f1754ff8f26c74f996
notuglyboy <569925642@qq.com>
2018-02-22 23:29:28 +0800

    'add

--- server/MyServer/testapp/models.py
+++ server/MyServer/testapp/models.py
@@ -5,13 +5,26 @@ class MyUser(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
-
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     link = models.URLField()
     descript =  models.CharField(max_length=100,null=True)
     content = models.TextField()
 
+class Commemt(models.Model):
+    
+    username = models.CharField(max_length=20)
+    content = models.TextField()
+    time = models.DateTimeField()
+    headimg = models.CharField(max_length=50)
+    
+class Reply(models.Model):
+    username = models.CharField(max_length=20)
+    content = models.TextField()
+    time = models.DateTimeField()
+    headimg = models.CharField(max_length=50)
+    to = models.CharField(max_length=20)
+    
 class testmedel(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
